829f4762f1dea0a1b73850ecc5f1370f07d62018
chunmeng <chunmeng.heng@greenwavesystems.com>
2020-05-10 10:42:35 +0800

    Add filter variables and hook up signals

--- linssid-app/ViewFilterDialog.cpp
+++ linssid-app/ViewFilterDialog.cpp
@@ -21,9 +21,22 @@ ViewFilterDialog::ViewFilterDialog(QWidget *parent, QObject *filterProxy)
     connect(widget.checkBox24G, SIGNAL(stateChanged(int)), this, SLOT(bandChanged(int)));
     connect(widget.checkBoxChannel, SIGNAL(stateChanged(int)), this, SLOT(channelChanged(int)));
     connect(widget.checkBoxSSID, SIGNAL(stateChanged(int)), this, SLOT(ssidChanged(int)));
+    connect(widget.checkBoxMAC, &QCheckBox::stateChanged, [this](int state) {
+                bool enabled = (state == Qt::CheckState::Checked) ? true : false;
+                options_.byMac = enabled;
+                widget.lineEditMAC->setEnabled(enabled);
+                emit filterUpdated(options_);
+            });
     // lineEditChannel returnPressed() and editingFinished()
     connect(widget.lineEditChannel, SIGNAL(editingFinished()), this, SLOT(channelTextChanged()));
     connect(widget.lineEditSSID, SIGNAL(editingFinished()), this, SLOT(ssidTextChanged()));
+    connect(widget.lineEditMAC, &QLineEdit::editingFinished, [this]() {
+                if (options_.mac == widget.lineEditMAC->text().toStdString())
+                    return;
+                DebugLog(AppLogger) << "Filter mac changed: " << widget.lineEditMAC->text().toStdString();
+                options_.mac = widget.lineEditMAC->text().toStdString();
+                emit filterUpdated(options_);
+            });
     // connect to filter model
     connect(this, SIGNAL(filterUpdated(FilterState)), filterProxy, SLOT(setFilter(FilterState)));
 }
@@ -39,11 +52,13 @@ void ViewFilterDialog::initUiStates(const FilterState& opt)
     widget.lineEditChannel->setEnabled(opt.byChannel);
     widget.lineEditChannel->setText(QString::fromStdString(opt.channels));
     widget.checkBoxSSID->setChecked(opt.bySsid);
+    widget.checkBoxMAC->setChecked(opt.byMac);
     // @FIXME: Does not behave the same as regex101 and still accept 6d number. Maybe this is over engineering, and a simple [0-9,-] would do... 
     // https://regex101.com/r/KwvbjH/3
     // v4 - ^([0-9]{1,3})([,-]+[0-9]{1,3})*([0-9]{1,3}[,])?$
     // v3 - ^([0-9]{1,3}){1}([,-]+[0-9]{1,3})*([0-9]{1,3}[,])?$
     widget.lineEditChannel->setValidator(new QRegExpValidator(QRegExp("^([0-9]{1,3})([,-]+[0-9]{1,3})*([0-9]{1,3}[,])?$"), widget.lineEditChannel));
+    widget.lineEditMAC->setValidator(new QRegExpValidator(QRegExp("^([0-9A-Fa-f?]{2}[:-]){5}([0-9A-Fa-f?]{2})$"), widget.lineEditMAC));
 }
 
 void ViewFilterDialog::bandGroupChanged(bool dontCare)
